fa93c4b3e58b5d983967d323241fc43e66fd493c
githukelvin <kelvingithu019@gmail.com>
2024-03-20 07:02:23 +0300

    first commit

---
+++ components/ContentBtn.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Component: ContentBtn
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/[slug].vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Page: [slug]
+  </div>
+</template>
+
+<style scoped></style>
